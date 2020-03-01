ae96a00cdfbe86108c894a9f71839e53a2d392b0
DiangeloHaaze <diangelohaaze.gmail.com>
2020-03-01 17:01:18 +0100

    V3.13.4.5

--- startbootstrap-modern-business-gh-pages/weizigproducten.php
+++ startbootstrap-modern-business-gh-pages/weizigproducten.php
@@ -1,195 +1,182 @@
 <?php
 session_start();
-if(!(isset($_SESSION['ingelogd']))){header("location:index.php");}
-if(isset($_POST["versturentwee"])){
-	include("php/controleupdateprod.php");
-	if ($goedkeuring){
-		include("php/weizigproduct.php");
-	}
+if(!isset($_SESSION["adminkey"])){
+    header('location:index.php');
+}
+else{
+
 }
  ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="">
+   <meta name="author" content="">
 
-  <title>Athena's Game</title>
+   <title>Athena's Game</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
- <!-- opmaak gemaakt door diangelo -->
-  <link href="css/diangelostyle.css" rel="stylesheet">
-  <!-- Custom styles for this template -->
-  <link href="css/modern-business.css" rel="stylesheet">
+   <!-- Bootstrap core CSS -->
+   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- opmaak gemaakt door diangelo -->
+   <link href="css/diangelostyle.css" rel="stylesheet">
+   <!-- Custom styles for this template -->
+   <link href="css/modern-business.css" rel="stylesheet">
 
-</head>
+ </head>
 
-<body>
+ <body>
 
-	<!-- De navigatie balk bovenaan de pagina op elke pagina. -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-  	<a class="navbar-brand" href="index.php">Athena's Game</a>
-  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-  	  <span class="navbar-toggler-icon"></span>
-  	</button>
-  	<div class="collapse navbar-collapse" id="navbarResponsive">
-  	  <ul class="navbar-nav ml-auto">
-  		<li class="nav-item">
-  		  <a class="nav-link" href="contact.php">Contact</a>
-  		</li>
-  	  <li class="nav-item">
-  		  <a class="nav-link" href="producten.php">Producten</a>
-  		</li>
-  		  <li class="nav-item">
-  		  <a class="nav-link" href="Subscribtie.php">Subscribtie</a>
-  		</li>
-  		   <?php
-  		 //kijkt of de gebruiker is ingelogd en indien ja komt dit tevoorschijn.
-  		 if(!isset($_SESSION["ingelogd"])){
-  			 ?>
-  		<li class="nav-item">
-  		  <a class="nav-link" href="registreer.php">Registreer</a>
-  		</li>
-  		  <li class="nav-item">
-  		  <a class="nav-link" href="Inloggen.php">inloggen</a>
-  		</li>
-  		  <?php }
-  		// en als je niet ingelogd ben krijg je dit te zien.
-  		else{ ?>
-  		  <li class="nav-item dropdown">
-  		  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  			<?php echo $_SESSION["gebruikernaam"]; ?>
-  		  </a>
-  		  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-  			<a class="dropdown-item" href="Weizigen.php">Aanpassen</a>
-  			<a class="dropdown-item" href="php/uitloggen.php">Uitloggen</a>
-  			  <?php
-  			// dit is alleen zichtbaar waneer de gebruiker een admin is.
-  			if($_SESSION["adminkey"] == true){?>
-  			<a class="dropdown-item" href="toonklanten.php">Gebruikers Bekijken</a>
-			<a class="dropdown-item" href="addproducten.php"> Producten Toevoegen</a>
-			<a class="dropdown-item" href="weizigproducten.php"> Producten Weizigen</a>
-  			  <?php } ?>
-  		  </div>
-  		</li>
-  		  <?php
-  		  }
-  		//hierna word het winkelwagentje getoont. Als er niets is ingevuld dan toont hij niet het aantel prodcuten maar als dit wel zo is geeft hij weer hoeveel er van 1 product aanwezig is in de winkelwagentje.
-  		  ?>
-  	  <li class="nav-item">
-  			<a class="notification" href="winkelwagen.php"><span class="glyphicon">&#x1f6d2;</span><span class="badge"><?php if($_SESSION["count"] != 0){echo $_SESSION["count"];} ?></span></a>
-  		</li>
-  	  </ul>
-  	</div>
-    </div>
-    </nav>
+ 	<!-- De navigatie balk bovenaan de pagina op elke pagina. -->
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+     <div class="container">
+   	<a class="navbar-brand" href="index.php">Athena's Game</a>
+   	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+   	  <span class="navbar-toggler-icon"></span>
+   	</button>
+   	<div class="collapse navbar-collapse" id="navbarResponsive">
+   	  <ul class="navbar-nav ml-auto">
+   		<li class="nav-item">
+   		  <a class="nav-link" href="contact.php">Contact</a>
+   		</li>
+   	  <li class="nav-item">
+   		  <a class="nav-link" href="producten.php">Producten</a>
+   		</li>
+   		  <li class="nav-item">
+   		  <a class="nav-link" href="Subscribtie.php">Subscribtie</a>
+   		</li>
+   		   <?php
+   		 //kijkt of de gebruiker is ingelogd en indien ja komt dit tevoorschijn.
+   		 if(!isset($_SESSION["ingelogd"])){
+   			 ?>
+   		<li class="nav-item">
+   		  <a class="nav-link" href="registreer.php">Registreer</a>
+   		</li>
+   		  <li class="nav-item">
+   		  <a class="nav-link" href="Inloggen.php">inloggen</a>
+   		</li>
+   		  <?php }
+   		// en als je niet ingelogd ben krijg je dit te zien.
+   		else{ ?>
+   		  <li class="nav-item dropdown">
+   		  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+   			<?php echo $_SESSION["gebruikernaam"]; ?>
+   		  </a>
+   		  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+   			<a class="dropdown-item" href="Weizigen.php">Aanpassen</a>
+   			<a class="dropdown-item" href="php/uitloggen.php">Uitloggen</a>
+   			  <?php
+   			// dit is alleen zichtbaar waneer de gebruiker een admin is.
+   			if($_SESSION["adminkey"] == true){?>
+   			<a class="dropdown-item" href="toonklanten.php">Gebruikers Bekijken</a>
+ 			<a class="dropdown-item" href="addproducten.php"> Producten Toevoegen</a>
+ 			<a class="dropdown-item" href="weizigproducten.php"> Producten Weizigen</a>
+   			  <?php } ?>
+   		  </div>
+   		</li>
+   		  <?php
+   		  }
+   		//hierna word het winkelwagentje getoont. Als er niets is ingevuld dan toont hij niet het aantel prodcuten maar als dit wel zo is geeft hij weer hoeveel er van 1 product aanwezig is in de winkelwagentje.
+   		  ?>
+   	  <li class="nav-item">
+   			<a class="notification" href="winkelwagen.php"><span class="glyphicon">&#x1f6d2;</span><span class="badge"><?php if($_SESSION["count"] != 0){echo $_SESSION["count"];} ?></span></a>
+   		</li>
+   	  </ul>
+   	</div>
+     </div>
+     </nav>
 
-  <!-- Page Content -->
-  <div class="container">
+   <!-- Page Content -->
+   <div class="container">
 
-    <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3">Registreren
-      <small>Gegevens</small>
-    </h1>
+     <!-- Page Heading/Breadcrumbs -->
+     <h1 class="mt-4 mb-3">Registreren
+       <small>Gegevens</small>
+     </h1>
 
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item">
-        <a href="index.php">Home</a>
-      </li>
-      <li class="breadcrumb-item active">Weizigen</li>
-    </ol>
+     <ol class="breadcrumb">
+       <li class="breadcrumb-item">
+         <a href="index.php">Home</a>
+       </li>
+       <li class="breadcrumb-item active">Weizigen</li>
+     </ol>
 
 
 
-    <div class="row">
-      <div class="col-lg-9 mb-4">
-        <h3>Wat wilt u weizigen?</h3>
+     <div class="row">
+       <div class="col-lg-9 mb-4">
+         <h3>Wat wilt u weizigen?</h3>
 
-		<form name="sentMessage" id="contactForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-          <div class="control-group form-group">
-            <div class="controls">
-				<?php
-		if(isset($_POST["versturen"]) && $_POST["productid"] != "" && isset($_POST["productid"])){
-				$_SESSION['productid'] = $_POST["productid"];
-				$mysqli = mysqli_connect('localhost', 'root', '', 'athenagames');
-				if(mysqli_connect_errno()) {trigger_error('Fout bij verbinding: '.$mysqli->error); }
+ 		<form name="sentMessage" id="contactForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+           <div class="control-group form-group">
+             <div class="controls">
+ 				<?php
+				if(!(isset($_POST["versturen"]) && $_POST["productnaam"])){
+ 				 ?>
+				 <div class="control-group form-group">
+	               <div class="controls">
+	                 <label>Naam van Het Product:</label>
+	                 <input type="text" class="form-control" name="productnaam" id="productnaam" value="<?php if(isset($_POST["productnaam"])){echo $_POST["productnaam"];} ?>">
+	               </div>
+	             </div>
+				 <hr>
+	             <button type="submit" name="versturen" class="btn btn-primary" id="sendMessageButton">Versturen</button><br><br>
+ 	  <?php }else{
+		  $mysqli = mysqli_connect('localhost', 'root', '', 'athenagames');
+		  if(mysqli_connect_errno()) {trigger_error('Fout bij verbinding: '.$mysqli->error); }
 
-				else
-				{
-					$prodid = mysqli_real_escape_string($mysqli, $_SESSION['productid']);
-					$sql = "SELECT productnaam, producttaal, beschrijving, prijsPstuk, linkfoto FROM tblproducten where productid = '$prodid'";
-					if($stmt = $mysqli->prepare($sql)){
-								if(!$stmt->execute()){
-									echo 'Het uitvoeren van de query is mislukt: '.$stmt->error.' in query: '.$sql;
-								}
-								else{
-									$stmt->bind_result($productnaam, $producttaal, $beschrijving, $prijsPstuk, $linkfoto);
-									while($stmt->fetch()){
-				 ?>
-				 <span>Productnaam:</span>
-				 <input type="text" class="form-control" name="productnaam" id="productnaam" value="<?php if(isset($_POST["productnaam"])){echo $_POST["productnaam"];} else{echo $productnaam;} ?>">
-				 <br>
-				 <span>Producttaal:</span>
-				 <input type="text" class="form-control" name="producttaal" id="producttaal" value="<?php if(isset($_POST["producttaal"])){echo $_POST["producttaal"];} else{echo $producttaal;} ?>">
-				 <br>
-				 <span>Beschrijving:</span>
-				 <input type="text" class="form-control" name="beschrijving" id="beschrijving" value="<?php if(isset($_POST["beschrijving"])){echo $_POST["beschrijving"];} else{echo $beschrijving;} ?>">
-				 <br>
-				 <span>Prijs per Stuk:</span>
-				 <input type="text" class="form-control" name="prijsPstuk" id="prijsPstuk" value="<?php if(isset($_POST["prijsPstuk"])){echo $_POST["prijsPstuk"];} else{echo $prijsPstuk;} ?>">
-				 <br>
-				 <span>Link naar de foto:</span>
-				 <input type="text" class="form-control" name="linkfoto" id="linkfoto" value="<?php if(isset($_POST["linkfotos"])){echo $_POST["linkfoto"];} else{echo $linkfoto;} ?>">
+		  else
+		  {
+			  $productnaam = mysqli_real_escape_string($mysqli,$_POST["productnaam"]);
+			  $sql_a = "SELECT productnaam, producttaal, beschrijving, prijsPstuk FROM tblproducten WHERE productnaam LIKE '$productnaam'";
 
-				 <div id="success"></div><hr>
-	             <button type="submit" name="versturentwee" class="btn btn-primary" id="sendMessageButton">Versturen</button><br><br>
+			  if($stmt = $mysqli->prepare($sql_a)){
+						  if(!$stmt->execute()){
+							  echo 'Het uitvoeren van de query is mislukt: '.$stmt->error.' in query: '.$sql;
+						  }
+						  else{
+							  $stmt->bind_result($productnaam, $producttaal, $beschrijving,$prijsPstuk);
+							  while($stmt->fetch()){
+	  ?>
+	  		<span>Productnaam:</span><br>
+			<input type="text" class="form-control" name="productnaam2" value="<?php if (isset($_POST["productnaam2"])) {
+				echo $_POST["productnaam2"];
+			}else {
+				echo $productnaam;
+			} ?>">
+  <?php }}}}} ?>
+          </form>
+       </div>
 
-	  <?php }}}}}else{ ?>
-		  <span>Productid:</span>
-		  <input type="text" class="form-control" name="productid" id="productid" value="<?php if(isset($_POST["productid"])){echo $_POST["productid"];} else{echo $_SESSION['productid'];} ?>">
-		  <div id="success"></div><hr>
-		  <button type="submit" name="versturen" class="btn btn-primary" id="sendMessageButton">Versturen</button><br><br>
-  <?php $terug = false;} ?>
-  <?php if(isset($gelukt)){ ?>
-			<span class="goed">Het Updaten was een success.</span>
-		<?php } if(isset($gefaalt)){ ?>
-			<span class="goed">Het Updaten was een faal.</span>
-		<?php } ?>
-         </form>
-      </div>
+     </div>
+ 	</div>
+     <!-- /.row -->
 
-    </div>
-	</div>
-    <!-- /.row -->
+   </div>
+ </div>
+   <!-- /.container -->
 
-  </div>
-</div>
-  <!-- /.container -->
+   <!-- Footer -->
+   <footer class="py-5 bg-dark">
+     <div class="container">
+       <p class="m-0 text-center text-white">Copyright &copy; Athena's Game 2019</p>
+     </div>
+     <!-- /.container -->
+   </footer>
 
-  <!-- Footer -->
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Athena's Game 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
+   <!-- Bootstrap core JavaScript -->
+   <script src="vendor/jquery/jquery.min.js"></script>
+   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <!-- Contact form JavaScript -->
+   <!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
+   <script src="js/jqBootstrapValidation.js"></script>
 
-  <!-- Contact form JavaScript -->
-  <!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-  <script src="js/jqBootstrapValidation.js"></script>
+   <!-- Javascript voor paswoord -->
+   <script src="js/paswoordvalidatie.js"></script>
+   <script src="js/foutcontrole.js"></script>
+ </body>
 
-  <!-- Javascript voor paswoord -->
-  <script src="js/paswoordvalidatie.js"></script>
-  <script src="js/foutcontrole.js"></script>
-</body>
-
-</html>
+ </html>
