f9ff6e6e5267de3fcf8c73e6d3422029bbc18f6b
DiangeloHaaze <diangelohaaze.gmail.com>
2020-05-10 20:19:26 +0200

    V3.13.6.4

--- startbootstrap-modern-business-gh-pages/php/facturatie_abbonnement.php
+++ startbootstrap-modern-business-gh-pages/php/facturatie_abbonnement.php
@@ -2,16 +2,16 @@
 if(isset($_POST["bevestigen"])){
 	switch ($_SESSION["actie"]) {
 		case '2':
-		$sql_c = "UPDATE tblklanten SET klantabonnement = '2' WHERE gebruikersnaam = '$username'";
+		$sql_b = "UPDATE tblklanten SET klantabonnement = '2' WHERE gebruikersnaam = '$username'";
 			break;
 		case '3':
-		$sql_c = "UPDATE tblklanten SET klantabonnement = '3' WHERE gebruikersnaam = '$username'";
+		$sql_b = "UPDATE tblklanten SET klantabonnement = '3' WHERE gebruikersnaam = '$username'";
 			break;
 		case '4':
-		$sql_c = "UPDATE tblklanten SET klantabonnement = '4' WHERE gebruikersnaam = '$username'";
+		$sql_b = "UPDATE tblklanten SET klantabonnement = '4' WHERE gebruikersnaam = '$username'";
 			break;
 	}
-	if($mysqli->query($sql_c)==true){
+	if($mysqli->query($sql_b)==true){
 		header("location:index.php");
 	}
 }
--- startbootstrap-modern-business-gh-pages/php/opzeggenabbo.php
+++ startbootstrap-modern-business-gh-pages/php/opzeggenabbo.php
@@ -1,3 +1,12 @@
 <?php
-
+if( isset($_GET["opzeggen"]) && $_GET["opzeggen"] == "goed"){
+$mysqli = mysqli_connect('localhost', 'root', '', 'athenagames');
+if(mysqli_connect_errno()) {trigger_error('Fout bij verbinding: '.$mysqli->error); }
+else
+{
+$username = $_SESSION["gebruikernaam"];
+$sql = "UPDATE tblklanten SET klantabonnement = '1' WHERE gebruikersnaam = '$username'";
+if($mysqli->query($sql)==true){}
+}
+}
  ?>
